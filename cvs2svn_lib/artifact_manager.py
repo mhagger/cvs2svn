c00d200a31d8d6b3eb70b999873ba57fe02be821
mhagger <mhagger@be7e6eca-30d4-0310-a8e5-ac0d63af7087>
2006-04-29 19:58:08 +0000

    Added the notion of an Artifact and an ArtifactManager.
    Eventually the ArtifactManager will be used to manage the cleanup of
    persistent artifacts in a way that is more flexible and
    module-friendly, and works for pass-by-pass conversions.  For now it
    is not used at all.
    
    * cvs2svn_lib/artifact_manager.py: New file.
    
    
    git-svn-id: http://cvs2svn.tigris.org/svn/cvs2svn/trunk@1872 be7e6eca-30d4-0310-a8e5-ac0d63af7087

---
+++ cvs2svn_lib/artifact_manager.py
@@ -0,0 +1,205 @@
+# (Be in -*- python -*- mode.)
+#
+# ====================================================================
+# Copyright (c) 2000-2006 CollabNet.  All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.  The terms
+# are also available at http://subversion.tigris.org/license-1.html.
+# If newer versions of this license are posted there, you may use a
+# newer version instead, at your option.
+#
+# This software consists of voluntary contributions made by many
+# individuals.  For exact contribution history, see the revision
+# history and logs, available at http://cvs2svn.tigris.org/.
+# ====================================================================
+
+"""This module manages the artifacts produced by conversion passes."""
+
+
+import os
+
+from boolean import *
+from common import FatalException
+from context import Ctx
+from log import Log
+
+
+class Artifact:
+  """An artifact that can be created, used across cvs2svn passes, then
+  cleaned up."""
+
+  def __init__(self, name):
+    self.name = name
+
+    # A map { which_pass : None } of passes that need this artifact.
+    # This field is maintained by ArtifactManager.
+    self._passes_needed = { }
+
+  def cleanup(self):
+    """This artifact is no longer needed; clean it up."""
+
+    pass
+
+  def __str__(self):
+    return self.name
+
+
+class TempFileArtifact(Artifact):
+  """A temporary file that can be used across cvs2svn passes."""
+
+  def __init__(self, basename):
+    self.name = basename
+    self.filename = os.path.join(Ctx().tmpdir, basename)
+
+  def cleanup(self):
+    Log().write(Log.VERBOSE, "Deleting", self.filename)
+    os.unlink(self.filename)
+
+
+class ArtifactManager:
+  """Manager artifacts that are created by one pass but needed by others.
+
+  This class is responsible for cleaning up artifacts once they are no
+  longer needed.  The trick is that cvs2svn can be run pass by pass,
+  so not all passes might be executed during a specific program run.
+
+  To use this class:
+
+  - Call register_artifact() or register_temp_file() for all possible
+    artifacts (even those that should have been created by previous
+    cvs2svn runs).
+
+  - Call register_artifact_needed() or register_temp_file_needed() for
+    any artifact that are needed by any pass (even those passes that
+    won't be executed during this cvs2svn run).
+
+  Then, in pass order:
+
+  - Call pass_skipped() for any passes that were already executed
+    during a previous cvs2svn run.
+
+  - Call pass_done() after a pass is executed, to allow any artifacts
+    that won't be needed anymore to be cleaned up.
+
+  - Call pass_deferred() for any passes that have been deferred to a
+    future cvs2svn run.
+
+  Finally:
+
+  - Call check_clean() to verify that all artifacts have been
+    accounted for."""
+
+  def __init__(self):
+    # A map { artifact_name : artifact } of known artifacts.
+    self._artifacts = { }
+
+    # A map { pass : set_of_artifacts }, where set_of_artifacts is a
+    # map { artifact : None } of artifacts needed by the pass.
+    self._pass_needs = { }
+
+  def register_artifact(self, which_pass, artifact):
+    """Register a new ARTIFACT for management by this class.
+    WHICH_PASS is the pass that creates ARTIFACT, and is also assumed
+    to need it."""
+
+    if artifact.name in self._artifacts:
+      raise FatalException('Artifact %s already registered' % (artifact,))
+    self._artifacts[artifact.name] = artifact
+    self.register_artifact_needed(which_pass, artifact.name)
+
+  def register_temp_file(self, which_pass, basename):
+    """Register a temporary file with base name BASENAME as an
+    artifact.  Return the filename of the temporary file."""
+
+    artifact = TempFileArtifact(basename)
+    self.register_artifact(which_pass, artifact)
+    return artifact.filename
+
+  def get_artifact(self, artifact_name):
+    """Return the artifact with the specified name.  If the artifact
+    does not currently exist, raise a FatalException."""
+
+    try:
+      return self._artifacts[artifact_name]
+    except KeyError:
+      raise FatalException('Artifact %s is not available' % (artifact_name,))
+
+  def get_temp_file(self, basename):
+    """Return the filename of the temporary file with the specified BASENAME.
+
+    If the temporary file is not an existing, registered
+    TempFileArtifact, raise a FatalException."""
+
+    return get_artifact(basename).filename
+
+  def register_artifact_needed(self, which_pass, artifact_name):
+    """Register that WHICH_PASS needs the artifact named ARTIFACT_NAME.
+    An artifact with this name must already have been registered."""
+
+    artifact = self.get_artifact(artifact_name)
+    artifact._passes_needed[which_pass] = None
+    self._pass_needs.setdefault(which_pass, {})[artifact] = None
+
+  def register_temp_file_needed(self, which_pass, basename):
+    """Register that the temporary file with base name BASENAME is
+    needed by WHICH_PASS."""
+
+    self.register_artifact_needed(which_pass, basename)
+
+  def _unregister_artifacts(self, which_pass):
+    """Unregister any artifacts that were needed for WHICH_PASS.
+
+    Return a list of artifacts that are no longer needed at all."""
+
+    try:
+      artifacts = self._pass_needs[which_pass].keys()
+    except KeyError:
+      # No artifacts were needed for that pass:
+      return
+
+    del self._pass_needs[which_pass]
+
+    unneeded_artifacts = []
+    for artifact in artifacts:
+      del artifact._passes_needed[which_pass]
+      if not artifact._passes_needed:
+        del self._artifacts[artifact.name]
+        unneeded_artifacts.append(artifact)
+
+    return unneeded_artifacts
+
+  def pass_skipped(self, which_pass):
+    """WHICH_PASS was executed during a previous cvs2svn run.
+
+    Its artifacts were created then, and any artifacts that would
+    normally be cleaned up after this pass have already been cleaned
+    up."""
+
+    self._unregister_artifacts(which_pass)
+
+  def pass_done(self, which_pass):
+    """WHICH_PASS is done.  Clean up all artifacts that are no longer
+    needed."""
+
+    for artifact in self._unregister_artifacts(which_pass):
+      artifact.cleanup()
+
+  def pass_deferred(self, which_pass):
+    """WHICH_PASS is being deferred until a future cvs2svn run.
+    Unregister any artifacts that would be cleaned up during
+    WHICH_PASS."""
+
+    self._unregister_artifacts(which_pass)
+
+  def check_clean(self):
+    """All passes have been processed.  Check that all artifacts have
+    been accounted for.  (This is mainly a consistency check, that no
+    artifacts were registered under nonexistent passes.)"""
+
+    if self._artifacts:
+      raise FatalException(
+          'The following artifacts were not cleaned up:\n    %s\n' %
+          ('\n    '.join([artifact.name for artifact in self._artifacts])))
+
+

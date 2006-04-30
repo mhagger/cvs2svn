5c6eb74ab6415f8e64609a4e29f91898ea385166
mhagger <mhagger@be7e6eca-30d4-0310-a8e5-ac0d63af7087>
2006-04-30 14:16:05 +0000

    Moved FillSource class to new fill_source module.
    * cvs2svn_lib/fill_source.py: New module, with class FillSource (moved
      from cvs2svn).
    
    * cvs2svn: Import from new module.
    
    
    git-svn-id: http://cvs2svn.tigris.org/svn/cvs2svn/trunk@1913 be7e6eca-30d4-0310-a8e5-ac0d63af7087

---
+++ cvs2svn_lib/fill_source.py
@@ -0,0 +1,49 @@
+# (Be in -*- python -*- mode.)
+#
+# ====================================================================
+# Copyright (c) 2000-2006 CollabNet.  All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.  The terms
+# are also available at http://subversion.tigris.org/license-1.html.
+# If newer versions of this license are posted there, you may use a
+# newer version instead, at your option.
+#
+# This software consists of voluntary contributions made by many
+# individuals.  For exact contribution history, see the revision
+# history and logs, available at http://cvs2svn.tigris.org/.
+# ====================================================================
+
+"""This module contains the FillSource class."""
+
+
+from boolean import *
+
+
+class FillSource:
+  """Representation of a fill source used by the symbol filler in
+  SVNRepositoryMirror."""
+
+  def __init__(self, prefix, node):
+    """Create an unscored fill source with a prefix and a key."""
+
+    self.prefix = prefix
+    self.node = node
+    self.score = None
+    self.revnum = None
+
+  def set_score(self, score, revnum):
+    """Set the SCORE and REVNUM."""
+
+    self.score = score
+    self.revnum = revnum
+
+  def __cmp__(self, other):
+    """Comparison operator used to sort FillSources in descending
+    score order."""
+
+    if self.score is None or other.score is None:
+      raise TypeError, 'Tried to compare unscored FillSource'
+    return cmp(other.score, self.score)
+
+

6edf0a3e9d6622f8b2a8cd4026aae36a983251fb
maxb <maxb@be7e6eca-30d4-0310-a8e5-ac0d63af7087>
2006-05-22 18:48:49 +0000

    Merge r2071 to the 1.3.x branch: Improvements to dist.sh, to fully utilize Python distutils, avoiding the need to unpack, tweak, and repack the tarball generated by them.
    git-svn-id: http://cvs2svn.tigris.org/svn/cvs2svn/branches/1.3.x@2213 be7e6eca-30d4-0310-a8e5-ac0d63af7087

--- dist.sh
+++ dist.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 
 # Build a cvs2svn distribution.
 
@@ -16,28 +17,19 @@ if echo ${WC_REV} | grep -q -e '[^0-9]'; then
 fi
 
 # Clean up anything that might have been left from a previous run.
-rm -rf dist MANIFEST cvs2svn-${VERSION} ${DIST_FULL}
+rm -rf dist MANIFEST ${DIST_BASE} ${DIST_FULL} cvs2svn-dist.sh-backup
+
+# Tweak cvs2svn to embed the proper version number.
+mv cvs2svn cvs2svn-dist.sh-backup
+sed -e "s/^VERSION = .*/VERSION = '${VERSION}'/" \
+  < cvs2svn-dist.sh-backup > cvs2svn
 
 # Build the dist, Python's way.
 ./setup.py sdist
 mv dist/${DIST_FULL} .
 
-# Unfortunately, building the dist Python's way doesn't seem to give
-# us an obvious method for including subdirs.  So, we rewire it!
-tar zxf ${DIST_FULL}
-rm ${DIST_FULL}
-svn export -q test-data ${DIST_BASE}/test-data
-svn export -q svntest ${DIST_BASE}/svntest
-svn export -q www ${DIST_BASE}/www
-# Oh, and while we're at it, let's fix cvs2svn's version number.
-sed -e "s/^VERSION = .*/VERSION = '${VERSION}'/" < cvs2svn > cvs2svn.tmp
-mv cvs2svn.tmp ${DIST_BASE}/cvs2svn
-chmod a+x ${DIST_BASE}/cvs2svn
-cp cvs2svn.1 ${DIST_BASE}
-tar zcf ${DIST_FULL} ${DIST_BASE}
-rm -rf ${DIST_BASE}
-
 # Clean up after this run.
+mv cvs2svn-dist.sh-backup cvs2svn
 rm -rf dist MANIFEST
 
 # We're outta here.

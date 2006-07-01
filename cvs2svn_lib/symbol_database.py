8d7fe5f2be2f879e07400c4ce9fbb46264f9adca
mhagger <mhagger@be7e6eca-30d4-0310-a8e5-ac0d63af7087>
2006-07-01 22:11:41 +0000

    Add the ability to look up symbol names by id and vice versa.
    (This is still unused.)
    
    * cvs2svn_lib/symbol_database.py (SymbolDatabase): Update docstring.
    
      (SymbolDatabase._symbols): New member.
    
      (SymbolDatabase.get_id, SymbolDatabase.get_name): New methods.
    
      (SymbolDatabase.__contains__): Add docstring.
    
    
    git-svn-id: http://cvs2svn.tigris.org/svn/cvs2svn/trunk@2465 be7e6eca-30d4-0310-a8e5-ac0d63af7087

--- cvs2svn_lib/symbol_database.py
+++ cvs2svn_lib/symbol_database.py
@@ -48,15 +48,22 @@ class TagSymbol(Symbol):
 class SymbolDatabase:
   """Read-only access to symbol database.
 
-  Records are name -> symbol, where symbol is a Symbol instance.  The
-  whole database is read into memory upon construction."""
+  The primary lookup mechanism is name -> symbol, where symbol is a
+  Symbol instance.  The whole database is read into memory upon
+  construction."""
 
   def __init__(self):
+    # A map { id : Symbol }
+    self._symbols = {}
+
+    # A map { name : Symbol }
     self._symbols_by_name = {}
+
     db = PDatabase(
         artifact_manager.get_temp_file(config.SYMBOL_DB), DB_OPEN_READ)
     for name in db.keys():
       symbol = db[name]
+      self._symbols[symbol.id] = symbol
       self._symbols_by_name[name] = symbol
 
   def get_symbol(self, name):
@@ -67,7 +74,23 @@ class SymbolDatabase:
 
     return self._symbols_by_name.get(name)
 
+  def get_id(self, name):
+    """Return the id of the symbol with the specified NAME.
+
+    Raise a KeyError if there is no such symbol."""
+
+    return self._symbols_by_name[name].id
+
+  def get_name(self, id):
+    """Return the name of the symbol with the specified ID.
+
+    Raise a KeyError if there is no such symbol."""
+
+    return self._symbols[id].name
+
   def __contains__(self, name):
+    """Return True iff NAME is a symbol being included in the conversion."""
+
     return name in self._symbols_by_name
 
   def collate_symbols(self, names):

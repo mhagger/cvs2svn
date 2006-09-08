0c5949777b4d7165e6588210560bc9fe72bbb38a
mhagger <mhagger@be7e6eca-30d4-0310-a8e5-ac0d63af7087>
2006-09-08 21:30:03 +0000

    Base destruction on rcsparse instead of the hacked-together file processing.
    This has the side effect of throwing out some info that is not
    understood by rcsparse and potentially changing the whitespace
    formatting.
    
    * contrib/destroy_repository.py (read_chunks, read_merged_chunks):
      Remove functions.
    
      (DestroyerFilterSink): New class.
    
      (FileDestroyer.destroy_file): Use rcsparse and DestroyerFilterSink
      instead of custom processing code.
    
    
    git-svn-id: http://cvs2svn.tigris.org/svn/cvs2svn/trunk@2707 be7e6eca-30d4-0310-a8e5-ac0d63af7087

--- contrib/destroy_repository.py
+++ contrib/destroy_repository.py
@@ -81,6 +81,10 @@ import traceback
 sys.path.insert(0, os.path.dirname(os.path.dirname(sys.argv[0])))
 
 from cvs2svn_lib.key_generator import KeyGenerator
+import cvs2svn_rcsparse
+from rcs_file_filter import WriteRCSFileSink
+from rcs_file_filter import FilterSink
+
 
 tmpdir = 'destroy_repository-tmp'
 
@@ -88,101 +92,6 @@ file_key_generator = KeyGenerator(1)
 
 def get_tmp_filename():
     return os.path.join(tmpdir, 'f%07d.tmp' % file_key_generator.gen_id())
-
-
-def read_chunks(filename):
-    """Generate '@'-delimited chunks of text from FILENAME.
-
-    Each yielded value contains all of the file contents up until the
-    next '@'.  The '@' are never included in the output.  The last
-    yield value is the part of the file following the last '@'."""
-
-    f = open(filename, 'rb')
-    buffer = []
-    while True:
-        s = f.read(4096)
-        if not s:
-            # That was the end of the file.  Yield whatever is left in
-            # buffer (even if the buffer is empty).
-            yield ''.join(buffer)
-            return
-        while True:
-            i = s.find('@')
-            if i == -1:
-                buffer.append(s)
-                break
-
-            buffer.append(s[:i])
-            s = s[i + 1:]
-            retval = ''.join(buffer)
-            buffer = []
-            yield retval
-
-
-def read_merged_chunks(filename):
-    """Generate the chunks from a properly-formed RCS file.
-
-    'Properly formed' means that it contains an even number of
-    '@'-characters.  A single '@' starts a quoted string.  While in a
-    quoted string, a double-'@' stands for a '@' within the string,
-    and a single-'@' stands for the end of the string.
-
-    This generator yields
-
-        unquoted, quoted, unquoted, quoted, unquoted, ...
-
-    where 'unquoted' are literal file contents between the quoted
-    strings, and 'quoted' are the contents of quoted strings including
-    '@@' for any enclosed '@' characters.  This routine will always
-    yield an odd number of chunks (or raise a RuntimeError if the RCS
-    file was malformed)."""
-
-    chunk_generator = read_chunks(filename)
-
-    # Yield the first chunk (before any '@'):
-    try:
-        yield chunk_generator.next()
-
-        # Each iteration of this loop produces one merged quoted
-        # string and the non-quoted part that comes after it:
-        while True:
-            # The file is positioned at the first chunk of a quoted string
-            # (or end of file).  First read all chunks that belong to this
-            # quoted string into chunks_to_merge.
-
-            # The following line is allowed to raise StopIteration; it
-            # simply means that the end of the file has been reached:
-            try:
-                chunks_to_merge = [chunk_generator.next()]
-            except StopIteration:
-                return
-
-            while True:
-                next_chunk = chunk_generator.next()
-
-                if next_chunk:
-                    # The quoted string is over and next_chunk is what
-                    # comes next.  First yield the quoted string:
-                    retval = '@@'.join(chunks_to_merge)
-                    chunks_to_merge = None
-                    yield retval
-
-                    # Now yield the non-quoted stuff that follows it:
-                    yield next_chunk
-
-                    break
-
-                # An empty trunk following a quoted string
-                # indicates that the chunk continues:
-                chunks_to_merge.append(chunk_generator.next())
-
-    except StopIteration, e:
-        # This shouldn't happen--all legitimate StopIterations have
-        # been caught within the body of this try.
-        traceback.print_exc()
-        raise RuntimeError(
-            'RCS file has invalid quoting structure.\n%r\n' % e
-            )
 
 
 class LogSubstituter:
@@ -218,47 +127,35 @@ class LogSubstituter:
             return new_log
 
 
+class DestroyerFilterSink(FilterSink):
+    def __init__(self, log_substituter, sink):
+        FilterSink.__init__(self, sink)
+
+        self.log_substituter = log_substituter
+
+    def set_description(self, description):
+        FilterSink.set_description(self, '')
+
+    def set_revision_info(self, revision, log, text):
+        FilterSink.set_revision_info(
+            self, revision, self.log_substituter.get_log_substitution(log), ''
+            )
+
+
 class FileDestroyer:
     def __init__(self):
         self.log_substituter = LogSubstituter()
 
     def destroy_file(self, filename):
-        chunk_generator = read_merged_chunks(filename)
-
         tmp_filename = get_tmp_filename()
         f = open(tmp_filename, 'wb')
-
-        while True:
-            try:
-                unquoted = chunk_generator.next()
-            except StopIteration, e:
-                # This shouldn't happen--read_merged_chunks promises to yield
-                # an odd number of chunks.
-                raise RuntimeError('RCS file has invalid quoting structure')
-
-            f.write(unquoted)
-
-            try:
-                quoted = chunk_generator.next()
-            except StopIteration, e:
-                # No problem--this is a legitimate end-of-file:
-                break
-
-            # Now allow the contents of unquoted to affect the processing
-            # of quoted:
-
-            if unquoted.endswith('\ntext\n'):
-                quoted = ''
-            elif unquoted.endswith('\ndesc\n'):
-                quoted = ''
-            elif unquoted.endswith('\nlog\n'):
-                quoted = self.log_substituter.get_log_substitution(quoted)
-
-            # Now write the (possibly altered) quoted string:
-            f.write('@')
-            f.write(quoted)
-            f.write('@')
-
+        cvs2svn_rcsparse.parse(
+            open(filename, 'rb'),
+            DestroyerFilterSink(
+                self.log_substituter,
+                WriteRCSFileSink(f),
+                )
+            )
         f.close()
 
         # Replace the original file with the new one:

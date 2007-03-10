1c0d7a451e7fa6e80c03989a3211dd0c7705e16a
mhagger <mhagger@be7e6eca-30d4-0310-a8e5-ac0d63af7087>
2007-03-10 06:34:55 +0000

    Move the main() method to a new module.
    * cvs2svn_lib/main.py: New module, containing main() function taken
      from the cvs2svn file.
    
    * cvs2svn: Remove "if __name__ == '__main__'" line, as this file
      cannot be imported and isn't execfile()d anymore.
    
      (main): Remove function (moved to new main module).
    
    
    git-svn-id: http://cvs2svn.tigris.org/svn/cvs2svn/trunk@3331 be7e6eca-30d4-0310-a8e5-ac0d63af7087

---
+++ cvs2svn_lib/main.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# (Be in -*- python -*- mode.)
+#
+# ====================================================================
+# Copyright (c) 2000-2007 CollabNet.  All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.  The terms
+# are also available at http://subversion.tigris.org/license-1.html.
+# If newer versions of this license are posted there, you may use a
+# newer version instead, at your option.
+#
+# This software consists of voluntary contributions made by many
+# individuals.  For exact contribution history, see the revision
+# history and logs, available at http://cvs2svn.tigris.org/.
+# ====================================================================
+
+import os
+import errno
+import gc
+
+try:
+  # Try to get access to a bunch of encodings for use with --encoding.
+  # See http://cjkpython.i18n.org/ for details.
+  import iconv_codec
+except ImportError:
+  pass
+
+from cvs2svn_lib.boolean import *
+from cvs2svn_lib import config
+from cvs2svn_lib.common import FatalException
+from cvs2svn_lib.common import FatalError
+from cvs2svn_lib.run_options import RunOptions
+from cvs2svn_lib.context import Ctx
+from cvs2svn_lib.pass_manager import PassManager
+from cvs2svn_lib.passes import passes
+
+
+def main(progname, cmd_args):
+  # Disable garbage collection, as we try not to create any circular
+  # data structures:
+  gc.disable()
+
+  # Convenience var, so we don't have to keep instantiating this Borg.
+  ctx = Ctx()
+
+  pass_manager = PassManager(passes)
+
+  run_options = RunOptions(progname, cmd_args, pass_manager)
+
+  # Make sure the tmp directory exists.  Note that we don't check if
+  # it's empty -- we want to be able to use, for example, "." to hold
+  # tempfiles.  But if we *did* want check if it were empty, we'd do
+  # something like os.stat(ctx.tmpdir)[stat.ST_NLINK], of course :-).
+  if not os.path.exists(ctx.tmpdir):
+    os.mkdir(ctx.tmpdir)
+  elif not os.path.isdir(ctx.tmpdir):
+    raise FatalError(
+        "cvs2svn tried to use '%s' for temporary files, but that path\n"
+        "  exists and is not a directory.  Please make it be a directory,\n"
+        "  or specify some other directory for temporary files."
+        % (ctx.tmpdir,))
+
+  # But do lock the tmpdir, to avoid process clash.
+  try:
+    os.mkdir(os.path.join(ctx.tmpdir, 'cvs2svn.lock'))
+  except OSError, e:
+    if e.errno == errno.EACCES:
+      raise FatalError("Permission denied:"
+                       + " No write access to directory '%s'." % ctx.tmpdir)
+    if e.errno == errno.EEXIST:
+      raise FatalError(
+          "cvs2svn is using directory '%s' for temporary files, but\n"
+          "  subdirectory '%s/cvs2svn.lock' exists, indicating that another\n"
+          "  cvs2svn process is currently using '%s' as its temporary\n"
+          "  workspace.  If you are certain that is not the case,\n"
+          "  then remove the '%s/cvs2svn.lock' subdirectory."
+          % (ctx.tmpdir, ctx.tmpdir, ctx.tmpdir, ctx.tmpdir,))
+    raise
+
+  try:
+    if run_options.profiling:
+      import hotshot
+      prof = hotshot.Profile('cvs2svn.hotshot')
+      prof.runcall(
+          pass_manager.run, run_options.start_pass, run_options.end_pass)
+      prof.close()
+    else:
+      pass_manager.run(run_options.start_pass, run_options.end_pass)
+  finally:
+    try:
+      os.rmdir(os.path.join(ctx.tmpdir, 'cvs2svn.lock'))
+    except:
+      pass
+
+

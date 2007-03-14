32b74d4fd8d79bb7b5600763612a6c76d886c4bf
mhagger <mhagger@be7e6eca-30d4-0310-a8e5-ac0d63af7087>
2007-03-14 21:41:38 +0000

    Factor out function check_for_garbage().
    * cvs2svn_lib/pass_manager.py (check_for_garbage): New function, using
      code copied out of PassManager.run().
    
      (PassManager.run): Call new method.
    
    
    git-svn-id: http://cvs2svn.tigris.org/svn/cvs2svn/trunk@3356 be7e6eca-30d4-0310-a8e5-ac0d63af7087

--- cvs2svn_lib/pass_manager.py
+++ cvs2svn_lib/pass_manager.py
@@ -36,6 +36,25 @@ class InvalidPassError(FatalError):
         self, msg + '\nUse --help-passes for more information.')
 
 
+def check_for_garbage():
+  # We've turned off the garbage collector because we shouldn't
+  # need it (we don't create circular dependencies) and because it
+  # is therefore a waste of time.  So here we check for any
+  # unreachable objects and generate a debug-level warning if any
+  # occur:
+  gc.set_debug(gc.DEBUG_SAVEALL)
+  gc_count = gc.collect()
+  if gc_count:
+    if Log().is_on(Log.DEBUG):
+      Log().debug(
+          'INTERNAL: %d unreachable object(s) were garbage collected:'
+          % (gc_count,)
+          )
+      for g in gc.garbage:
+        Log().debug('    %s' % (g,))
+    del gc.garbage[:]
+
+
 class PassManager:
   """Manage a list of passes that can be executed separately or all at once.
 
@@ -130,22 +149,7 @@ class PassManager:
       # longer needed:
       artifact_manager.pass_done(the_pass)
 
-      # We've turned off the garbage collector because we shouldn't
-      # need it (we don't create circular dependencies) and because it
-      # is therefore a waste of time.  So here we check for any
-      # unreachable objects and generate a debug-level warning if any
-      # occur:
-      gc.set_debug(gc.DEBUG_SAVEALL)
-      gc_count = gc.collect()
-      if gc_count:
-        if Log().is_on(Log.DEBUG):
-          Log().debug(
-              'INTERNAL: %d unreachable object(s) were garbage collected:'
-              % (gc_count,)
-              )
-          for g in gc.garbage:
-            Log().debug('    %s' % (g,))
-        del gc.garbage[:]
+      check_for_garbage()
 
     # Tell the artifact manager about passes that are being deferred:
     for the_pass in self.passes[index_end:]:

dcc9fb2e9181034c545d6891e3bd32de08051737
mhagger <mhagger@be7e6eca-30d4-0310-a8e5-ac0d63af7087>
2007-07-31 21:07:05 +0000

    Introduce support for branching with git output.
    * cvs2svn_lib/git_output_option.py (GitOutputOption.check): Remove
      requirement of --trunk-only.
    
      (GitOutputOption.process_post_commit): Don't raise
      NotImplementedError, to allow partial testing.
    
      (GitOutputOption._get_source_groups): New method.
    
      (GitOutputOption.process_branch_commit): Implement method.
    
    * test-data/main-cvsrepos/cvs2svn-git.options: Don't set
      --trunk-only.  Add symbol strategy to convert all tags to branches
      (because tags are not yet supported).
    
    
    git-svn-id: http://cvs2svn.tigris.org/svn/cvs2svn/trunk@3924 be7e6eca-30d4-0310-a8e5-ac0d63af7087

--- cvs2svn_lib/git_output_option.py
+++ cvs2svn_lib/git_output_option.py
@@ -19,9 +19,12 @@
 
 from __future__ import generators
 
+import bisect
+
 from cvs2svn_lib.boolean import *
 from cvs2svn_lib.set_support import *
 from cvs2svn_lib import config
+from cvs2svn_lib.common import InternalError
 from cvs2svn_lib.common import FatalError
 from cvs2svn_lib.common import warning_prefix
 from cvs2svn_lib.log import Log
@@ -33,6 +36,7 @@ from cvs2svn_lib.cvs_item import CVSRevisionModification
 from cvs2svn_lib.cvs_item import CVSRevisionDelete
 from cvs2svn_lib.cvs_item import CVSRevisionNoop
 from cvs2svn_lib.output_option import OutputOption
+from cvs2svn_lib.svn_revision_range import RevisionScores
 
 
 class GitOutputOption(OutputOption):
@@ -58,10 +62,6 @@ class GitOutputOption(OutputOption):
 
   def check(self):
     Log().warn('!!!!! WARNING: Git output is highly experimental !!!!!')
-    if not Ctx().trunk_only:
-      raise FatalError(
-          'Git output is currently only supported with --trunk-only'
-          )
     if Ctx().cross_project_commits:
       raise FatalError(
           'Git output is not supported with cross-project commits'
@@ -155,7 +155,42 @@ class GitOutputOption(OutputOption):
     self.f.write('\n')
 
   def process_post_commit(self, svn_commit):
-    raise NotImplementedError() # FIXME
+    pass # @@@ raise NotImplementedError() # FIXME
+
+  def _get_source_groups(self, svn_commit):
+    """Return groups of sources for SVN_COMMIT.
+
+    SVN_COMMIT is an instance of SVNSymbolCommit.  Yield tuples
+    (source_lod, svn_revnum, cvs_symbols) where source_lod is the line
+    of development and svn_revnum is the revision that should serve as
+    a source, and cvs_symbols is a list of CVSSymbolItems that can be
+    copied from that source.  The groups are returned in arbitrary
+    order."""
+
+    # Get a map {CVSSymbol : SVNRevisionRange}:
+    range_map = self._symbolings_reader.get_range_map(svn_commit)
+
+    # range_map, split up into one map per LOD; i.e., {LOD :
+    # {CVSSymbol : SVNRevisionRange}}:
+    lod_range_maps = {}
+
+    for (cvs_symbol, range) in range_map.iteritems():
+      lod_range_map = lod_range_maps.get(cvs_symbol.source_lod)
+      if lod_range_map is None:
+        lod_range_map = {}
+        lod_range_maps[cvs_symbol.source_lod] = lod_range_map
+      lod_range_map[cvs_symbol] = range
+
+    for (lod, lod_range_map) in lod_range_maps.iteritems():
+      while lod_range_map:
+        revision_scores = RevisionScores(lod_range_map.values())
+        (revnum, score) = revision_scores.get_best_revnum()
+        cvs_symbols = []
+        for (cvs_symbol, range) in lod_range_map.items():
+          if revnum in range:
+            cvs_symbols.append(cvs_symbol)
+            del lod_range_map[cvs_symbol]
+        yield (lod, revnum, cvs_symbols)
 
   def _get_source_mark(self, source_lod, revnum):
     """Return the mark active at REVNUM on SOURCE_LOD."""
@@ -166,7 +201,54 @@ class GitOutputOption(OutputOption):
     return mark
 
   def process_branch_commit(self, svn_commit):
-    raise NotImplementedError() # FIXME
+    author = svn_commit.get_author()
+    if author is None:
+      author = Ctx().username
+    try:
+      author = Ctx().utf8_encoder(author)
+    except UnicodeError:
+      Log().warn('%s: problem encoding author:' % warning_prefix)
+      Log().warn("  author: '%s'" % (author,))
+
+    log_msg = svn_commit.get_log_msg()
+    try:
+      log_msg = Ctx().utf8_encoder(log_msg)
+    except UnicodeError:
+      Log().warn('%s: problem encoding log message:' % warning_prefix)
+      Log().warn("  log:    '%s'" % log_msg.rstrip())
+
+    self.f.write('commit refs/heads/%s\n' % (svn_commit.symbol.name,))
+    self.f.write(
+        'mark :%d\n'
+        % (self._create_commit_mark(svn_commit.symbol, svn_commit.revnum),)
+        )
+    self.f.write(
+        'committer %s <%s> %d +0000\n' % (author, author, svn_commit.date,)
+        )
+    self.f.write('data %d\n' % (len(log_msg),))
+    self.f.write('%s\n' % (log_msg,))
+
+    source_groups = list(self._get_source_groups(svn_commit))
+    for (source_lod, source_revnum, cvs_branches,) in source_groups:
+      self.f.write(
+          'merge :%d\n'
+          % (self._get_source_mark(source_lod, source_revnum),)
+          )
+
+    for (source_lod, source_revnum, cvs_branches,) in source_groups:
+      for cvs_branch in cvs_branches:
+        if cvs_branch.cvs_file.executable:
+          mode = '100755'
+        else:
+          mode = '100644'
+
+        self.f.write(
+            'M %s :%d %s\n'
+            % (mode, cvs_branch.revision_recorder_token,
+               cvs_branch.cvs_file.cvs_path,)
+            )
+
+    self.f.write('\n')
 
   def process_tag_commit(self, svn_commit):
     raise NotImplementedError() # FIXME

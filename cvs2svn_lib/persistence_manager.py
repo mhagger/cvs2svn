a1fcb64e13b0142b68bdc1f3d127dad5fe7b651b
mhagger <mhagger@be7e6eca-30d4-0310-a8e5-ac0d63af7087>
2007-08-20 21:02:13 +0000

    Remove unneeded import.
    (Fixing pychecker warnings.)
    
    * cvs2svn_lib/persistence_manager.py: Remove unneeded import.
    
    
    git-svn-id: http://cvs2svn.tigris.org/svn/cvs2svn/trunk@4045 be7e6eca-30d4-0310-a8e5-ac0d63af7087

--- cvs2svn_lib/persistence_manager.py
+++ cvs2svn_lib/persistence_manager.py
@@ -17,8 +17,6 @@
 """This module contains class PersistenceManager."""
 
 
-import bisect
-
 from cvs2svn_lib.boolean import *
 from cvs2svn_lib import config
 from cvs2svn_lib.common import DB_OPEN_NEW

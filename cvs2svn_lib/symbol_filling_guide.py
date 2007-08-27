80c278a843fb4095639a9fbf269c26c4102de4fe
mhagger <mhagger@be7e6eca-30d4-0310-a8e5-ac0d63af7087>
2007-08-27 20:52:38 +0000

    Use FillSource objects to hold the LOD trees in _SymbolFillingGuide.
    * cvs2svn_lib/symbol_filling_guide.py (FillSource): Update docstring.
    
      (FillSource.revnum, FillSource.score): Fill these members in
      compute_best_revnum rather than in the constructor.
    
      (FillSource._get_node, FillSource._set_node, FillSource.print_tree,
      FillSource._print_subtree): New methods.
    
      (FillSource.compute_best_revnum): Member renamed from
      _get_best_revnum(); also set revnum and score members here.
    
      (FillSourceSet): Update docstring.
    
      (FillSourceSet.__init__): Call FillSource.compute_best_revnum() on
      each source.  Update comments.
    
      (_SymbolFillingGuide): Update docstring.
    
      (_SymbolFillingGuide._fill_sources): New member, replacing
      _node_trees.
    
      (_SymbolFillingGuide.__init__): Initialize new member.
    
      (_SymbolFillingGuide._get_node,
      _SymbolFillingGuide.print_node_trees,
      _SymbolFillingGuide._print_node_tree): Remove methods.
    
      (_SymbolFillingGuide.get_source_set): Work with new member.
    
      (_SymbolFillingGuide.print_fill_sources): New method.
    
    
    git-svn-id: http://cvs2svn.tigris.org/svn/cvs2svn/trunk@4087 be7e6eca-30d4-0310-a8e5-ac0d63af7087

--- cvs2svn_lib/symbol_filling_guide.py
+++ cvs2svn_lib/symbol_filling_guide.py
@@ -33,11 +33,20 @@ from cvs2svn_lib.svn_revision_range import RevisionScores
 class FillSource:
   """Representation of a fill source.
 
-  A fill source is a directory (either trunk or a branches
-  subdirectory) that can be used as a source for a symbol, along with
-  the self-computed score for the source.  FillSources can be
-  compared; the comparison is such that it sorts FillSources in
-  descending order by score (higher score implies smaller).
+  A fill source describes an LOD that can be used as a source for a
+  particular CVSPath in a symbol that is being filled.
+
+  This class holds a SVNRevisionRange instance for each CVSFile that
+  has to be filled within the subtree of the repository rooted at
+  self._cvs_path.  The SVNRevisionRange objects are stored in a tree
+  in which the directory nodes are dictionaries mapping CVSPaths to
+  subnodes and the leaf nodes are the SVNRevisionRange objects telling
+  for what range of revisions the leaf could serve as a source.
+
+  FillSource objects are able to compute the score for the source.
+  FillSources can be compared; the comparison is such that it sorts
+  FillSources in descending order by score (higher score implies
+  smaller).
 
   These objects are used by the symbol filler in SVNRepositoryMirror."""
 
@@ -66,9 +75,27 @@ class FillSource:
     self.lod = lod
     self._node_tree = node_tree
     self._preferred_source = preferred_source
-    self.revnum, self.score = self._get_best_revnum()
 
-  def _get_best_revnum(self):
+  def _set_node(self, cvs_file, svn_revision_range):
+    parent_node = self._get_node(cvs_file.parent_directory, create=True)
+    parent_node[cvs_file] = svn_revision_range
+
+  def _get_node(self, cvs_path, create=False):
+    if cvs_path == self._cvs_path:
+      return self._node_tree
+    else:
+      parent_node = self._get_node(cvs_path.parent_directory, create=create)
+      try:
+        return parent_node[cvs_path]
+      except KeyError:
+        if create:
+          node = {}
+          parent_node[cvs_path] = node
+          return node
+        else:
+          raise
+
+  def compute_best_revnum(self):
     """Determine the best subversion revision number to use when
     copying the source tree beginning at this source.
 
@@ -94,13 +121,13 @@ class FillSource:
       raise FatalError(
           "failed to find a revision to copy from when copying %s"
           % self._symbol.name)
-    return best_revnum, best_score
+    self.revnum, self.score = best_revnum, best_score
 
   def _get_revision_ranges(self, node):
     """Return a list of all the SVNRevisionRanges at and under NODE.
 
     Include duplicates.  This is a helper method used by
-    _get_best_revnum()."""
+    compute_best_revnum()."""
 
     if isinstance(node, SVNRevisionRange):
       # It is a leaf node.
@@ -141,19 +168,49 @@ class FillSource:
                   and self.lod == self._preferred_source.lod) \
            or cmp(self.lod, other.lod)
 
+  def print_tree(self):
+    """Print all nodes to sys.stdout.
+
+    This method is included for debugging purposes."""
+
+    self._print_subtree(self._node_tree, self._cvs_path, indent_depth=0)
+
+  def _print_subtree(self, node, cvs_path, indent_depth=0):
+    """Print all nodes that are rooted at NODE to sys.stdout.
+
+    INDENT_DEPTH is used to indent the output of recursive calls.
+    This method is included for debugging purposes."""
+
+    if isinstance(node, SVNRevisionRange):
+      print "TREE:", " " * (indent_depth * 2), cvs_path, node
+    else:
+      print "TREE:", " " * (indent_depth * 2), cvs_path
+      for sub_path, sub_node in node.items():
+        self._print_subtree(sub_node, sub_path, indent_depth + 1)
+
 
 class FillSourceSet:
-  """A set of FillSources for a given symbol and path."""
+  """A set of FillSources for a given Symbol and CVSPath.
+
+  self._sources holds a FillSource object for each LineOfDevelopment
+  that holds some sources needed to fill the subpath rooted at
+  self.cvs_path for symbol self._symbol.  The source with the highest
+  score should be used to fill self.cvs_path.  Then the caller should
+  descend into sub-nodes to see if their 'best revnum' differs from
+  their parent's and if it does, take appropriate actions to 'patch
+  up' the subtrees."""
 
   def __init__(self, symbol, cvs_path, sources):
-    # The symbol that the sources are for:
+    # The symbol being filled:
     self._symbol = symbol
 
-    # The CVSPath that is being processed:
+    # The CVSPath that is being described:
     self.cvs_path = cvs_path
 
     # A list of sources, sorted in descending order of score.
     self._sources = sources
+    for source in self._sources:
+      source.compute_best_revnum()
     self._sources.sort()
 
   def __nonzero__(self):
@@ -184,25 +241,10 @@ class FillSourceSet:
 
 
 class _SymbolFillingGuide:
-  """A tree holding the sources that can be copied to fill a symbol.
+  """A description of the sources that can be copied to fill a symbol.
 
-  The class holds a node tree for each LineOfDevelopment that is the
-  source LOD of a CVSSymbol in an SVNCommit.  The directory nodes in
-  each tree are dictionaries mapping CVSPaths to subnodes.  A leaf
-  node exists for the source of each CVSSymbol, and thus should be
-  copied to the symbol in this commit.  The leaves themselves are
-  SVNRevisionRange objects telling for what range of revisions the
-  leaf could serve as a source.
-
-  self._node_trees holds the root nodes of one directory tree per
-  LineOfDevelopment.  By walking self._node_trees and calling
-  self._get_best_revnum() on each node, the caller can determine what
-  subversion revision number to copy the path corresponding to that
-  node from.  self._node_trees should be treated as read-only.
-
-  The caller can then descend to sub-nodes to see if their 'best
-  revnum' differs from their parent's and if it does, take appropriate
-  actions to 'patch up' the subtrees."""
+  The class holds a FillSource instance for each LineOfDevelopment
+  that is the source LOD of a CVSSymbol in an SVNCommit."""
 
   def __init__(self, symbol, range_map):
     """Initialize a _SymbolFillingGuide for SYMBOL.
@@ -214,30 +256,26 @@ class _SymbolFillingGuide:
 
     self.symbol = symbol
 
-    # A map { LOD : tree } for each LOD, where tree is a map { CVSPath
-    # : node }, and subnodes have the same form.  Leaves are
-    # SVNRevisionRange instances.
-    self._node_trees = { }
+    # A map { LOD : FillSource } for each LOD containing sources that
+    # need to be filled.
+    self._fill_sources = {}
 
+    root_cvs_directory = Ctx()._cvs_file_db.get_file(
+        self.symbol.project.root_cvs_directory_id
+        )
     for cvs_symbol, svn_revision_range in range_map.items():
-      node_tree = self._node_trees.setdefault(cvs_symbol.source_lod, {})
-      cvs_file = cvs_symbol.cvs_file
-      parent_node = self._get_node(node_tree, cvs_file.parent_directory)
-      parent_node[cvs_file] = svn_revision_range
+      source_lod = cvs_symbol.source_lod
+      try:
+        fill_source = self._fill_sources[source_lod]
+      except KeyError:
+        fill_source = FillSource(
+            root_cvs_directory, self.symbol, source_lod, {}
+            )
+        self._fill_sources[source_lod] = fill_source
 
-    #self.print_node_trees()
+      fill_source._set_node(cvs_symbol.cvs_file, svn_revision_range)
 
-  def _get_node(self, node_tree, cvs_path):
-    """Return the node for CVS_PATH within NODE_TREE.
-
-    NODE_TREE is a node tree as described above.  CVS_PATH is a
-    CVSPath instance.  Create new nodes as needed."""
-
-    if cvs_path.parent_directory is None:
-      return node_tree
-    else:
-      parent_node = self._get_node(node_tree, cvs_path.parent_directory)
-      return parent_node.setdefault(cvs_path, {})
+    #self.print_fill_sources()
 
   def get_source_set(self):
     """Return a FillSourceSet for the root path for this symbolic name.
@@ -248,38 +286,17 @@ class _SymbolFillingGuide:
     root_cvs_directory = Ctx()._cvs_file_db.get_file(
         self.symbol.project.root_cvs_directory_id
         )
-    sources = [
-        FillSource(root_cvs_directory, self.symbol, lod, node_tree)
-        for (lod, node_tree) in self._node_trees.iteritems()
-        ]
+    sources = self._fill_sources.values()
     return FillSourceSet(self.symbol, root_cvs_directory, sources)
 
-  def print_node_trees(self):
+  def print_fill_sources(self):
     print "TREE", "=" * 75
-    lods = self._node_trees.keys()
+    lods = self._fill_sources.keys()
     lods.sort()
     for lod in lods:
       print 'TREE LOD = %s' % (lod,)
-      self._print_node_tree(
-          self._node_trees[lod],
-          Ctx()._cvs_file_db.get_file(
-              self.symbol.project.root_cvs_directory_id
-              ),
-          )
+      self._fill_sources[lod].print_tree()
       print "TREE", "-" * 75
-
-  def _print_node_tree(self, node, cvs_directory, indent_depth=0):
-    """Print all nodes that are rooted at NODE to sys.stdout.
-
-    INDENT_DEPTH is used to indent the output of recursive calls.
-    This method is included for debugging purposes."""
-
-    if isinstance(node, SVNRevisionRange):
-      print "TREE:", " " * (indent_depth * 2), cvs_directory, node
-    else:
-      print "TREE:", " " * (indent_depth * 2), cvs_directory
-      for sub_directory, sub_node in node.items():
-        self._print_node_tree(sub_node, sub_directory, (indent_depth + 1))
 
 
 def get_source_set(symbol, range_map):

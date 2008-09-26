fa990b99a0267bae276cf126704ba35255eaf8bf
mhagger <mhagger@be7e6eca-30d4-0310-a8e5-ac0d63af7087>
2008-09-26 06:56:54 +0000

    Trivial formatting changes.
    * contrib/git-move-tags.py: Trivial formatting changes throughout file
      to make it conform more closely to the rest of the project.
    
    
    git-svn-id: http://cvs2svn.tigris.org/svn/cvs2svn/trunk@4602 be7e6eca-30d4-0310-a8e5-ac0d63af7087

--- contrib/git-move-tags.py
+++ contrib/git-move-tags.py
@@ -2,13 +2,14 @@
 
 """Remove redundant tag fixup commits from a cvs2svn-converted git repository.
 
-Process each tag in a git repository. If the tagged commit is tree-wise
-identical with another tagged commit, the tag is moved to point at the
-other commit (i.e. tags pointing at identical content will all point at a
-single tag fixup commit).
+Process each tag in a git repository. If the tagged commit is
+tree-wise identical with another tagged commit, the tag is moved to
+point at the other commit (i.e., tags pointing at identical content
+will all point at a single tag fixup commit).
 
-Furthermore, if one of the parents of the tag fixup commit is identical to the
-tag fixup commit itself, then the tag is moved to the parent.
+Furthermore, if one of the parents of the tag fixup commit is
+identical to the tag fixup commit itself, then the tag is moved to the
+parent.
 
 The script is meant to be run against a repository converted by
 cvs2svn, since cvs2svn creates empty commits for some tags.
@@ -17,27 +18,33 @@ cvs2svn, since cvs2svn creates empty commits for some tags.
 
 from subprocess import Popen, PIPE, call
 
-# Cache trees we have already seen, and that are suitable targets for moved tags
+# Cache trees we have already seen, and that are suitable targets for
+# moved tags
 tree_cache = {} # tree SHA1 -> commit SHA1
 
 # Cache parent commit -> parent tree mapping
 parent_cache = {} # commit SHA1 -> tree SHA1
 
 def resolve_commit(commit):
-    """Return the tree object associated with the given commit"""
+    """Return the tree object associated with the given commit."""
+
     get_tree_cmd = ["git", "rev-parse", commit + "^{tree}"]
     tree = Popen(get_tree_cmd, stdout = PIPE).communicate()[0].strip()
     return tree
 
 def move_tag(tag, from_commit, to_commit):
-    """Move the given tag to the given commit"""
+    """Move the given tag to the given commit."""
+
     print "Moving tag %s from %s to %s..." % (tag, from_commit, to_commit),
     retcode = call(["git", "tag", "-f", tag, to_commit])
-    if retcode == 0: print "done"
-    else: print "FAILED"
+    if retcode == 0:
+        print "done"
+    else:
+        print "FAILED"
 
 def try_to_move_tag(tag, commit, tree, parents):
-    """Try to move the given tag to a separate commit (with identical tree)"""
+    """Try to move the given tag to a separate commit (with identical tree)."""
+
     if tree in tree_cache:
         # We have already found a suitable commit for this tree
         move_tag(tag, commit, tree_cache[tree])
@@ -45,10 +52,12 @@ def try_to_move_tag(tag, commit, tree, parents):
 
     # Try to move this tag to one of its commit's parents
     for p in parents:
-        if p not in parent_cache: # Not in cache
+        if p not in parent_cache:
+            # Not in cache
             parent_cache[p] = resolve_commit(p)
         p_tree = parent_cache[p]
-        if tree == p_tree: # We can move tag to parent p
+        if tree == p_tree:
+            # We can move tag to parent p
             move_tag(tag, commit, p)
             commit = p
             break
@@ -77,22 +86,30 @@ while True: # While get_tag_info process is still running
          commit, tree, parents,
          commit_alt, tree_alt, parents_alt) = line.split(chr(0))
         if objtype == "tag":
-            commit  = commit_alt
-            tree    = tree_alt
+            commit = commit_alt
+            tree = tree_alt
             parents = parents_alt
-        elif objtype != "commit": continue
+        elif objtype != "commit":
+            continue
 
-        if subject.startswith("This commit was manufactured by cvs2svn") or not subject:
+        if subject.startswith("This commit was manufactured by cvs2svn") \
+               or not subject:
             # We shall try to move this tag, if possible
             parent_list = []
-            if parents: parent_list = parents.split(" ")
-            for p in parent_list: assert len(p) == 40
+            if parents:
+                parent_list = parents.split(" ")
+            for p in parent_list:
+                assert len(p) == 40
             assert tag.startswith("refs/tags/")
             try_to_move_tag(tag[10:], commit, tree, parent_list)
         else:
             # We shall not move this tag, but it is a possible target
             # for other tags that we _do_ want to move
             tree_cache.setdefault(tree, commit)
-    if get_tag_info.poll() is not None: break # Break if no longer running
+
+    if get_tag_info.poll() is not None:
+        # Break if no longer running:
+        break
 
 assert get_tag_info.returncode == 0
+

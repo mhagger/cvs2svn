55647f5e52c81832a36887bd38217813b990bcce
mhagger <mhagger@be7e6eca-30d4-0310-a8e5-ac0d63af7087>
2008-11-30 06:38:31 +0000

    Never treat a delete revision as the original source of another revision.
    It is not possible to retrieve the contents of a deleted revision
    using CVS, so don't allow a deleted revision to be used as the
    "original source" of another revision.  Also, don't write blobs for
    deleted revisions.
    
    This fixes a bug in cvs2git output when using non-inline blobs,
    wherein a revision that followed a deleted revision usually had no
    contents.
    
    Suggested by: Erik Änggård <erik.anggard@gmail.com>
    
    * cvs2svn_lib/git_revision_recorder.py
      (GitRevisionRecorder._get_original_source): Don't consider
      CVSRevisionDeletes to be possible sources.  Update and improve
      docstring.
    
      (GitRevisionRecorder.record_fulltext): Don't record blobs for
      CVSRevisionDeletes.  Update and improve docstring.
    
    git-svn-id: http://cvs2svn.tigris.org/svn/cvs2svn/trunk@4642 be7e6eca-30d4-0310-a8e5-ac0d63af7087

--- cvs2svn_lib/git_revision_recorder.py
+++ cvs2svn_lib/git_revision_recorder.py
@@ -23,6 +23,7 @@ fulltext and possibly return a revision_recorder_token."""
 import itertools
 
 from cvs2svn_lib.symbol import Trunk
+from cvs2svn_lib.cvs_item import CVSRevisionDelete
 from cvs2svn_lib.cvs_item import CVSSymbol
 from cvs2svn_lib.fulltext_revision_recorder import FulltextRevisionRecorder
 from cvs2svn_lib.key_generator import KeyGenerator
@@ -42,32 +43,48 @@ class GitRevisionRecorder(FulltextRevisionRecorder):
     self._cvs_file_items = cvs_file_items
 
   def _get_original_source(self, cvs_rev):
-    """Return the first CVSRevision with the content of CVS_REV.
+    """Return the original source of the contents of CVS_REV.
 
-    'First' here refers to deltatext order; i.e., the very first
-    revision is HEAD on trunk, then backwards to the root of a branch,
-    then out to the tip of a branch.
+    Return the first non-delete CVSRevision with the same contents as
+    CVS_REV.  'First' here refers to deltatext order; i.e., the very
+    first revision is HEAD on trunk, then backwards to the root of a
+    branch, then out to the tip of a branch.
 
-    If there is no other CVSRevision that have the same content,
-    return CVS_REV itself."""
+    The candidates are all revisions along the CVS delta-dependency
+    chain until the next one that has a deltatext (inclusive).  Of the
+    candidates, CVSRevisionDeletes are disqualified because, even
+    though CVS records their contents, it is impossible to extract
+    their fulltext using commands like 'cvs checkout -p'.
+
+    If there is no other CVSRevision that has the same content, return
+    CVS_REV itself."""
+
+    # Keep track of the "best" source CVSRevision found so far:
+    best_source_rev = None
 
     for cvs_rev in itertools.chain(
           [cvs_rev], self._cvs_file_items.iter_deltatext_ancestors(cvs_rev)
           ):
+      if not isinstance(cvs_rev, CVSRevisionDelete):
+        best_source_rev = cvs_rev
+
       if cvs_rev.deltatext_exists:
         break
 
-    return cvs_rev
+    return best_source_rev
 
   def record_fulltext(self, cvs_rev, log, fulltext):
-    """Write the fulltext to a blob if it is original.
+    """Write the fulltext to a blob if it is original and not a delete.
 
-    To find the 'original' revision, we follow the CVS
-    delta-dependency chain backwards until we find a file that has a
-    deltatext.  The reason we go to this trouble is to avoid writing
-    the same file contents multiple times for a string of revisions
-    that don't have deltatexts (as, for example, happens with dead
-    revisions and imported revisions)."""
+    The reason we go to this trouble is to avoid writing the same file
+    contents multiple times for a string of revisions that don't have
+    deltatexts (as, for example, happens with dead revisions and
+    imported revisions)."""
+
+    if isinstance(cvs_rev, CVSRevisionDelete):
+      # There is no need to record a delete revision, and its token
+      # will never be needed:
+      return None
 
     source = self._get_original_source(cvs_rev)
 

1eb6d365341ed620901d510ded8d06c12b4b08d9
mhagger <mhagger@be7e6eca-30d4-0310-a8e5-ac0d63af7087>
2009-02-08 06:30:41 +0000

    * setup.py: Improve capitalization.
    git-svn-id: http://cvs2svn.tigris.org/svn/cvs2svn/trunk@4780 be7e6eca-30d4-0310-a8e5-ac0d63af7087

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     name = "cvs2svn",
     version = get_version(),
     description = "CVS to Subversion or git repository converter",
-    author = "The cvs2svn Team",
+    author = "The cvs2svn team",
     author_email = "<dev@cvs2svn.tigris.org>",
     url = "http://cvs2svn.tigris.org/",
     license = "Apache-style",

c5605cb31a21bb5dc71ec74de27560463f171f28
mhagger <mhagger@be7e6eca-30d4-0310-a8e5-ac0d63af7087>
2009-02-27 09:27:38 +0000

    Add information to setup.py to enable submission to PyPI.
    * setup.py: Add long_description and download_url.
    
    Patch by: Anatoly Techtonik <techtonik@gmail.com>
    
    git-svn-id: http://cvs2svn.tigris.org/svn/cvs2svn/trunk@4787 be7e6eca-30d4-0310-a8e5-ac0d63af7087

--- setup.py
+++ setup.py
@@ -22,7 +22,45 @@ setup(
     author = "The cvs2svn team",
     author_email = "<dev@cvs2svn.tigris.org>",
     url = "http://cvs2svn.tigris.org/",
+    download_url = "http://cvs2svn.tigris.org/servlets/ProjectDocumentList?folderID=2976",
     license = "Apache-style",
+    long_description = """cvs2svn_ is a tool for migrating a CVS repository to
+Subversion_ or git_. The main design goals are robustness and 100% data
+preservation. cvs2svn can convert just about any CVS repository we've ever
+seen, including gcc, Mozilla, FreeBSD, KDE, GNOME...
+
+.. _cvs2svn: http://cvs2svn.tigris.org
+.. _Subversion: http://svn.tigris.org
+.. _git: http://git-scm.com/
+
+cvs2svn infers what happened in the history of your CVS repository and
+replicates that history as accurately as possible in the target SCM. All
+revisions, branches, tags, log messages, author names, and commit dates are
+converted. cvs2svn deduces what CVS modifications were made at the same time,
+and outputs these modifications grouped together as changesets in the target
+SCM. cvs2svn also deals with many CVS quirks and is highly configurable.
+See the comprehensive `feature list`_.
+
+.. _feature list: http://cvs2svn.tigris.org/features.html
+.. _documentation: http://cvs2svn.tigris.org/cvs2svn.html
+
+Please read the documentation_ carefully before using cvs2svn.
+
+
+Latest development version
+--------------------------
+
+For general use, the most recent released version of cvs2svn is usually the
+best choice. However, if you want to use the newest cvs2svn features or if
+you're debugging or patching cvs2svn, you might want to use the trunk version
+(which is usually quite stable). To do so, use Subversion to check out a
+working copy from http://cvs2svn.tigris.org/svn/cvs2svn/trunk/ using a command
+like
+
+ svn co --username=guest http://cvs2svn.tigris.org/svn/cvs2svn/trunk cvs2svn-trunk
+
+(the password is empty; i.e., just press return).
+""",
     classifiers = [
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',

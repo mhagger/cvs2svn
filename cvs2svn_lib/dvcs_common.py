5645e0be3ddf55da8c6faa43b395b2e122833f89
mhagger <mhagger@be7e6eca-30d4-0310-a8e5-ac0d63af7087>
2009-08-25 20:34:59 +0000

    Factor DVCSRunOptions out of GitRunOptions.
    Patch by: Greg Ward <greg@gerg.ca>
    
    * cvs2svn_lib/dvcs_common.py: add class DVCSRunOptions
    * cvs2svn_lib/git_run_options.py:
      - make GitRunOptions a subclass of DVCSRunOptions
      - move constructor, set_project(), process_options() up to DVCSRunOptions
      - add some comments about opportunities to refactor
    
    git-svn-id: http://cvs2svn.tigris.org/svn/cvs2svn/trunk@4903 be7e6eca-30d4-0310-a8e5-ac0d63af7087

--- cvs2svn_lib/dvcs_common.py
+++ cvs2svn_lib/dvcs_common.py
@@ -18,9 +18,74 @@
 Git, Mercurial, or Bazaar).
 """
 
+import sys
+
 from cvs2svn_lib.common import FatalError
+from cvs2svn_lib.run_options import RunOptions
+from cvs2svn_lib.log import Log
 from cvs2svn_lib.context import Ctx
+from cvs2svn_lib.project import Project
 from cvs2svn_lib.output_option import OutputOption
+
+
+class DVCSRunOptions(RunOptions):
+  """Dumping ground for whatever is common to GitRunOptions and
+  HgRunOptions."""
+  def __init__(self, progname, cmd_args, pass_manager):
+    Ctx().cross_project_commits = False
+    Ctx().cross_branch_commits = False
+    RunOptions.__init__(self, progname, cmd_args, pass_manager)
+
+  def set_project(
+        self,
+        project_cvs_repos_path,
+        symbol_transforms=None,
+        symbol_strategy_rules=[],
+        ):
+    """Set the project to be converted.
+
+    If a project had already been set, overwrite it.
+
+    Most arguments are passed straight through to the Project
+    constructor.  SYMBOL_STRATEGY_RULES is an iterable of
+    SymbolStrategyRules that will be applied to symbols in this
+    project."""
+
+    symbol_strategy_rules = list(symbol_strategy_rules)
+
+    project = Project(
+        0,
+        project_cvs_repos_path,
+        symbol_transforms=symbol_transforms,
+        )
+
+    self.projects = [project]
+    self.project_symbol_strategy_rules = [symbol_strategy_rules]
+
+  def process_options(self):
+    # Consistency check for options and arguments.
+    if len(self.args) == 0:
+      self.usage()
+      sys.exit(1)
+
+    if len(self.args) > 1:
+      Log().error(error_prefix + ": must pass only one CVS repository.\n")
+      self.usage()
+      sys.exit(1)
+
+    cvsroot = self.args[0]
+
+    self.process_extraction_options()
+    self.process_output_options()
+    self.process_symbol_strategy_options()
+    self.process_property_setter_options()
+
+    # Create the project:
+    self.set_project(
+        cvsroot,
+        symbol_transforms=self.options.symbol_transforms,
+        symbol_strategy_rules=self.options.symbol_strategy_rules,
+        )
 
 
 class DVCSOutputOption(OutputOption):
--- cvs2svn_lib/git_run_options.py
+++ cvs2svn_lib/git_run_options.py
@@ -24,22 +24,21 @@ import codecs
 from cvs2svn_lib.version import VERSION
 from cvs2svn_lib.common import error_prefix
 from cvs2svn_lib.common import FatalError
-from cvs2svn_lib.log import Log
 from cvs2svn_lib.context import Ctx
-from cvs2svn_lib.run_options import not_both
+from cvs2svn_lib.dvcs_common import DVCSRunOptions
 from cvs2svn_lib.run_options import RunOptions
 from cvs2svn_lib.run_options import ContextOption
 from cvs2svn_lib.run_options import IncompatibleOption
 from cvs2svn_lib.run_options import authors
+from cvs2svn_lib.run_options import not_both
 from cvs2svn_lib.man_writer import ManWriter
-from cvs2svn_lib.project import Project
+from cvs2svn_lib.revision_manager import NullRevisionRecorder
+from cvs2svn_lib.revision_manager import NullRevisionExcluder
 from cvs2svn_lib.rcs_revision_manager import RCSRevisionReader
 from cvs2svn_lib.cvs_revision_manager import CVSRevisionReader
 from cvs2svn_lib.git_revision_recorder import GitRevisionRecorder
 from cvs2svn_lib.git_output_option import GitRevisionMarkWriter
 from cvs2svn_lib.git_output_option import GitOutputOption
-from cvs2svn_lib.revision_manager import NullRevisionRecorder
-from cvs2svn_lib.revision_manager import NullRevisionExcluder
 from cvs2svn_lib.fulltext_revision_recorder \
      import SimpleFulltextRevisionRecorderAdapter
 
@@ -88,14 +87,10 @@ see_also = [
   ]
 
 
-class GitRunOptions(RunOptions):
-  def __init__(self, progname, cmd_args, pass_manager):
-    Ctx().cross_project_commits = False
-    Ctx().cross_branch_commits = False
-    RunOptions.__init__(self, progname, cmd_args, pass_manager)
+class GitRunOptions(DVCSRunOptions):
 
   def _get_output_options_group(self):
-    group = RunOptions._get_output_options_group(self)
+    group = DVCSRunOptions._get_output_options_group(self)
 
     group.add_option(IncompatibleOption(
         '--blobfile', type='string',
@@ -129,6 +124,8 @@ class GitRunOptions(RunOptions):
 
     return group
 
+  # XXX shouldn't this be the same as -- or, better, shared with --
+  # SVNRunOptions?
   def _get_extraction_options_group(self):
     group = RunOptions._get_extraction_options_group(self)
 
@@ -178,6 +175,8 @@ class GitRunOptions(RunOptions):
         ).write_manpage(f)
     sys.exit(0)
 
+  # XXX not quite the same as same method in SVNRunOptions, but it
+  # probably should be
   def process_extraction_options(self):
     """Process options related to extracting data from the CVS
     repository."""
@@ -220,56 +219,3 @@ class GitRunOptions(RunOptions):
         # Optional map from CVS author names to git author names:
         author_transforms={}, # FIXME
         )
-
-  def set_project(
-        self,
-        project_cvs_repos_path,
-        symbol_transforms=None,
-        symbol_strategy_rules=[],
-        ):
-    """Set the project to be converted.
-
-    If a project had already been set, overwrite it.
-
-    Most arguments are passed straight through to the Project
-    constructor.  SYMBOL_STRATEGY_RULES is an iterable of
-    SymbolStrategyRules that will be applied to symbols in this
-    project."""
-
-    symbol_strategy_rules = list(symbol_strategy_rules)
-
-    project = Project(
-        0,
-        project_cvs_repos_path,
-        symbol_transforms=symbol_transforms,
-        )
-
-    self.projects = [project]
-    self.project_symbol_strategy_rules = [symbol_strategy_rules]
-
-  def process_options(self):
-    # Consistency check for options and arguments.
-    if len(self.args) == 0:
-      self.usage()
-      sys.exit(1)
-
-    if len(self.args) > 1:
-      Log().error(error_prefix + ": must pass only one CVS repository.\n")
-      self.usage()
-      sys.exit(1)
-
-    cvsroot = self.args[0]
-
-    self.process_extraction_options()
-    self.process_output_options()
-    self.process_symbol_strategy_options()
-    self.process_property_setter_options()
-
-    # Create the project:
-    self.set_project(
-        cvsroot,
-        symbol_transforms=self.options.symbol_transforms,
-        symbol_strategy_rules=self.options.symbol_strategy_rules,
-        )
-
-

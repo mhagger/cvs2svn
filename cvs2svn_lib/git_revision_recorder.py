8b175020721221720aa963209bc089221ed0f9c3
mhagger <mhagger@be7e6eca-30d4-0310-a8e5-ac0d63af7087>
2010-02-15 03:49:07 +0000

    Remove the GitRevisionRecorder._get_original_source() stuff.
    It is doubtful whether it saved much intermediate storage space, and
    (in the case of CVS keyword expansion) didn't give quite the correct
    revision contents.  It also is the only place that the original
    CVSFileItems are needed by any RevisionRecorder, so getting rid of
    this code simplifies bookkeeping.
    
    git-svn-id: http://cvs2svn.tigris.org/svn/cvs2svn/trunk@5051 be7e6eca-30d4-0310-a8e5-ac0d63af7087

--- cvs2svn_lib/git_revision_recorder.py
+++ cvs2svn_lib/git_revision_recorder.py
@@ -35,68 +35,21 @@ class GitRevisionRecorder(FulltextRevisionRecorder):
     self.dump_file = open(self.blob_filename, 'wb')
     self._mark_generator = KeyGenerator()
 
-  def start_file(self, cvs_file_items):
-    self._cvs_file_items = cvs_file_items
-
-  def _get_original_source(self, cvs_rev):
-    """Return the original source of the contents of CVS_REV.
-
-    Return the first non-delete CVSRevision with the same contents as
-    CVS_REV.  'First' here refers to deltatext order; i.e., the very
-    first revision is HEAD on trunk, then backwards to the root of a
-    branch, then out to the tip of a branch.
-
-    The candidates are all revisions along the CVS delta-dependency
-    chain until the next one that has a deltatext (inclusive).  Of the
-    candidates, CVSRevisionDeletes are disqualified because, even
-    though CVS records their contents, it is impossible to extract
-    their fulltext using commands like 'cvs checkout -p'.
-
-    If there is no other CVSRevision that has the same content, return
-    CVS_REV itself."""
-
-    # Keep track of the "best" source CVSRevision found so far:
-    best_source_rev = None
-
-    for cvs_rev in itertools.chain(
-          [cvs_rev], self._cvs_file_items.iter_deltatext_ancestors(cvs_rev)
-          ):
-      if not isinstance(cvs_rev, CVSRevisionDelete):
-        best_source_rev = cvs_rev
-
-      if cvs_rev.deltatext_exists:
-        break
-
-    return best_source_rev
-
   def record_fulltext(self, cvs_rev, log, fulltext):
-    """Write the fulltext to a blob if it is original and not a delete.
-
-    The reason we go to this trouble is to avoid writing the same file
-    contents multiple times for a string of revisions that don't have
-    deltatexts (as, for example, happens with dead revisions and
-    imported revisions)."""
+    """Write the revision fulltext to a blob if it is not dead."""
 
     if isinstance(cvs_rev, CVSRevisionDelete):
       # There is no need to record a delete revision, and its token
       # will never be needed:
       return None
 
-    source = self._get_original_source(cvs_rev)
-
-    if source.id == cvs_rev.id:
-      # Revision is its own source; write it out:
-      mark = self._mark_generator.gen_id()
-      self.dump_file.write('blob\n')
-      self.dump_file.write('mark :%d\n' % (mark,))
-      self.dump_file.write('data %d\n' % (len(fulltext),))
-      self.dump_file.write(fulltext)
-      self.dump_file.write('\n')
-      return mark
-    else:
-      # Return as revision_recorder_token the CVSRevision.id of the
-      # original source revision:
-      return source.revision_recorder_token
+    mark = self._mark_generator.gen_id()
+    self.dump_file.write('blob\n')
+    self.dump_file.write('mark :%d\n' % (mark,))
+    self.dump_file.write('data %d\n' % (len(fulltext),))
+    self.dump_file.write(fulltext)
+    self.dump_file.write('\n')
+    return mark
 
   def finish_file(self, cvs_file_items):
     # Determine the original source of each CVSSymbol, and store it as
@@ -105,8 +58,6 @@ class GitRevisionRecorder(FulltextRevisionRecorder):
       if isinstance(cvs_item, CVSSymbol):
         cvs_source = cvs_item.get_cvs_revision_source(cvs_file_items)
         cvs_item.revision_recorder_token = cvs_source.revision_recorder_token
-
-    del self._cvs_file_items
 
   def finish(self):
     self.dump_file.close()

f5988d73655fef55a2994b4a4542f9be83bbd502
mhagger <mhagger@be7e6eca-30d4-0310-a8e5-ac0d63af7087>
2010-06-16 04:29:31 +0000

    Simplify code.
    git-svn-id: http://cvs2svn.tigris.org/svn/cvs2svn/trunk@5194 be7e6eca-30d4-0310-a8e5-ac0d63af7087

--- cvs2svn_lib/dumpfile_delegate.py
+++ cvs2svn_lib/dumpfile_delegate.py
@@ -254,10 +254,7 @@ class DumpfileDelegate(SVNRepositoryDelegate):
     # treat .cvsignore as a directory property
     dir_path, basename = path_split(cvs_rev.get_svn_path())
     if basename == '.cvsignore':
-      ignore_vals = generate_ignores(data)
-      ignore_contents = '\n'.join(ignore_vals)
-      if ignore_contents:
-        ignore_contents += '\n'
+      ignore_contents = ''.join((s + '\n') for s in generate_ignores(data))
       ignore_contents = ('K 10\nsvn:ignore\nV %d\n%s\n' % \
                          (len(ignore_contents), ignore_contents))
       ignore_contents += 'PROPS-END\n'

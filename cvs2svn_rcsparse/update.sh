fd843df30d55efc78d3b9f1398db2fb80c13494c
mhagger <mhagger@be7e6eca-30d4-0310-a8e5-ac0d63af7087>
2011-05-24 08:39:08 +0000

    Convert cvs2svn_rcsparse/update.sh to Python.
    Rename it to update.
    
    git-svn-id: http://cvs2svn.tigris.org/svn/cvs2svn/trunk@5352 be7e6eca-30d4-0310-a8e5-ac0d63af7087

--- cvs2svn_rcsparse/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -ex
-
-VIEWVC_REPOS="http://viewvc.tigris.org/svn/viewvc"
-
-# Update the rcsparse library from ViewVC
-svn export --force "$VIEWVC_REPOS/trunk/lib/vclib/ccvs/rcsparse" .
-

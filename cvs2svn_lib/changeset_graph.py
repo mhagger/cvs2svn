75199a08f719ae9843a5c72d304f57ccc6a810c1
Michael Haggerty <mhagger@alum.mit.edu>
2021-11-12 11:08:48 +0100

    Modify a bunch of file-level license blurbs to remove dead links

--- cvs2svn_lib/changeset_graph.py
+++ cvs2svn_lib/changeset_graph.py
@@ -4,14 +4,11 @@
 # Copyright (c) 2006-2008 CollabNet.  All rights reserved.
 #
 # This software is licensed as described in the file COPYING, which
-# you should have received as part of this distribution.  The terms
-# are also available at http://subversion.tigris.org/license-1.html.
-# If newer versions of this license are posted there, you may use a
-# newer version instead, at your option.
+# you should have received as part of this distribution.
 #
 # This software consists of voluntary contributions made by many
 # individuals.  For exact contribution history, see the revision
-# history and logs, available at http://cvs2svn.tigris.org/.
+# history and logs.
 # ====================================================================
 
 """The changeset dependency graph."""
